8e7dfb8b344c563da5f153d5c685878ac84653ef
Sergey Ilyuhin <ilyuhin-s@mail.ru>
2024-06-10 01:07:59 +0500

    grg

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
-
+import {
+  Flex,
+  Text,
+  Button,
+  Box,
+  Card,
+  SegmentedControl,
+} from "@radix-ui/themes";
+WebApp.MainButton;
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Flex gap="3" direction="column">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,26 +25,41 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button
+      <Text size="7" weight="bold">
+        Vite + React
+      </Text>
+
+      <Flex align='stretch' direction="column" gap="4">
+        <SegmentedControl.Root defaultValue="inbox" radius="full">
+          <SegmentedControl.Item value="inbox">Inbox</SegmentedControl.Item>
+          <SegmentedControl.Item value="drafts">Drafts</SegmentedControl.Item>
+          <SegmentedControl.Item value="sent">Sent</SegmentedControl.Item>
+        </SegmentedControl.Root>
+      </Flex>
+
+      <Box maxWidth="350px">
+        <Card asChild>
+          <Flex gap="3" direction="column">
+            <Text as="div" color="gray" size="2">
+              Start building your next project in minutes
+            </Text>
+            <Button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+          </Flex>
+        </Card>
+      </Box>
+      <Button
+        size={"4"}
+        radius="large"
+        variant="surface"
         onClick={() =>
           WebApp.showAlert(`Hello World! Current count is ${count}`)
         }
       >
         Show Alert 2
-      </button>
-    </>
+      </Button>
+    </Flex>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import WebApp from "@twa-dev/sdk";
+import { Theme } from "@radix-ui/themes";
 
+import "@radix-ui/themes/styles.css";
 WebApp.backgroundColor = "#000000";
 WebApp.headerColor = "#000000";
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>
 );
